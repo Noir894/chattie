ea80485fea33761b5a9e3a34ea8bd995d1f2b57a
Nikhil Pandey <nikhilpandey7916@gmail.com>
2026-03-29 20:06:57 +0530

    Added HTML code

--- app.py
+++ app.py
@@ -1,96 +1,113 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from flask_socketio import SocketIO, emit
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'supersecretkey123'
 socketio = SocketIO(app)
+
+# ✅ FIXED USER DB (IMPORTANT)
+users = {
+    "test": generate_password_hash("123")
+}
 
 waiting_user = None
 partners = {}
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users:
+            return "User already exists"
+
+        users[username] = generate_password_hash(password)
+        return redirect('/login')
+
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user_password = users.get(username)
+
+        if user_password and check_password_hash(user_password, password):
+            session['user'] = username
+            return redirect('/')
+        else:
+            return "Invalid credentials"
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/login')
+
+
 @app.route('/')
 def index():
+    if 'user' not in session:
+        return redirect('/login')
     return render_template('index.html')
+
 
 @socketio.on('join')
 def on_join():
     global waiting_user
+
+    user = session.get('user')
+    if not user:
+        return
+
     if waiting_user is None:
         waiting_user = request.sid
-        emit('waiting', {'msg': 'Stranger dhundh rahe hain...'})
+        emit('waiting', {'msg': f'{user}, stranger dhundh rahe hain...'})
     else:
         partner = waiting_user
         waiting_user = None
+
         partners[request.sid] = partner
         partners[partner] = request.sid
-        emit('connected', {'msg': 'Stranger mil gaya! Baat karo 😊'})
-        emit('connected', {'msg': 'Stranger mil gaya! Baat karo 😊'}, to=partner)
+
+        emit('connected', {'msg': 'Stranger mil gaya!'} )
+        emit('connected', {'msg': 'Stranger mil gaya!'}, to=partner)
+
 
 @socketio.on('message')
 def on_message(data):
     partner = partners.get(request.sid)
     if partner:
-        emit('message', {'msg': data['msg'], 'user': 'Stranger'}, to=partner)
-        emit('message', {'msg': data['msg'], 'user': 'Tum'})
+        msg = data.get('msg', '').strip()
+        if not msg:
+            return
 
-@socketio.on('image')
-def on_image(data):
-    partner = partners.get(request.sid)
-    if partner:
-        emit('image', {'img': data['img'], 'user': 'Stranger'}, to=partner)
-        emit('image', {'img': data['img'], 'user': 'Tum'})
+        emit('message', {'msg': msg, 'user': 'Stranger'}, to=partner)
+        emit('message', {'msg': msg, 'user': 'Tum'})
 
-@socketio.on('webrtc_offer')
-def on_offer(data):
-    partner = partners.get(request.sid)
-    if partner:
-        emit('webrtc_offer', data, to=partner)
-
-@socketio.on('webrtc_answer')
-def on_answer(data):
-    partner = partners.get(request.sid)
-    if partner:
-        emit('webrtc_answer', data, to=partner)
-
-@socketio.on('webrtc_ice')
-def on_ice(data):
-    partner = partners.get(request.sid)
-    if partner:
-        emit('webrtc_ice', data, to=partner)
 
 @socketio.on('next')
 def on_next():
     global waiting_user
+
     partner = partners.get(request.sid)
+
     if partner:
         emit('disconnected', {'msg': 'Stranger chala gaya!'}, to=partner)
         partners.pop(partner, None)
+
     partners.pop(request.sid, None)
     waiting_user = None
+
     emit('waiting', {'msg': 'Naya stranger dhundh rahe hain...'})
 
-@socketio.on('typing')
-def on_typing():
-    partner = partners.get(request.sid)
-    if partner:
-        emit('typing', {}, to=partner)
-
-@socketio.on('stop_typing')
-def on_stop_typing():
-    partner = partners.get(request.sid)
-    if partner:
-        emit('stop_typing', {}, to=partner)
-
-@socketio.on('disconnect')
-def on_disconnect():
-    global waiting_user
-    partner = partners.get(request.sid)
-    if partner:
-        emit('disconnected', {'msg': 'Stranger ne connection tod diya! 😢'}, to=partner)
-        partners.pop(partner, None)
-    partners.pop(request.sid, None)
-    if waiting_user == request.sid:
-        waiting_user = None
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
